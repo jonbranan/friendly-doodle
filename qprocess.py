976e36316c1fcf48a052ef7850124202e3bc4288
jblu <jonbranan@gmail.com>
2022-07-14 22:09:57 -0500

    adjusted logging around age calculation

--- qprocess.py
+++ qprocess.py
@@ -17,8 +17,7 @@ def torprocessor(self):
         if canidate['ratio'] < float(1.05):
             self.tl.debug(f'{canidate["name"]} is below a 1.05 ratio({canidate["ratio"]})')
             if canidate['added_on'] + self.config["age"] <= self.t.time():
-                self.tl.debug(f'Calculation: {canidate["added_on"] + self.config["age"]}')
-                self.tl.debug(f'Comparison: {self.t.time()}')
+                self.tl.debug(f'{canidate["name"]} Seconds old: {self.t.time() - self.config["age"] - canidate["added_on"]}')
                 self.torrent_hash_delete_list.append(canidate['infohash_v1'])
                 self.tl.info(f'Submitted {canidate["name"]} for deletion from the protected list.') 
         if canidate['ratio'] >= float(1.05):
@@ -34,5 +33,6 @@ def printprocessor(self):
     self.tl.info(f'Total torrents set for deletion: {len(self.torrent_hash_delete_list)}')
 
 def tordelete(self):
+    self.tl.debug('Hash list submitted for deletion:')
     self.tl.debug(self.torrent_hash_delete_list)
     self.qbt_client.torrents_delete(True, self.torrent_hash_delete_list)
